04cbcc97a8918905cc4d0788d3a9405dd25d842f
shahharshil46 <shahharshil46@gmail.com>
2023-10-10 03:01:57 -0700

    Updates gradle build settings, works with JDK 17

--- src/main/java/com/shahharshil46/redis/leaderboardwithredisdemo/LeaderboardwithredisdemoApplication.java
+++ src/main/java/com/shahharshil46/redis/leaderboardwithredisdemo/LeaderboardwithredisdemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeaderboardwithredisdemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LeaderboardwithredisdemoApplication.class, args);
 	}
 
